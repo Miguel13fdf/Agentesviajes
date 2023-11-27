96cb74ca0bc0380bcdc505aec541dbef2aa53096
Miguel13fdf <miguel.haro.est@tecazuay.edu.ec>
2023-11-26 19:27:59 -0500

    Update AES_ENCRYPTION.java

--- src/java/intermediario/AES_ENCRYPTION.java
+++ src/java/intermediario/AES_ENCRYPTION.java
@@ -5,75 +5,83 @@
  */
 package intermediario;
 
+import java.security.MessageDigest;
+import java.util.Arrays;
 import java.util.Base64;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author USUARIO
  */
 public class AES_ENCRYPTION {
-    private SecretKey key;
-    private final int KEY_SIZE = 128;
-    private final int DATA_LENGTH = 128;
-    private Cipher encryptionCipher;
+     private String LLAVE = "ISTAAESEncrypt";
 
-    public AES_ENCRYPTION() {
-        init();
-    }
-
-    public AES_ENCRYPTION(SecretKey key, Cipher encryptionCipher) {
-        this.key = key;
-        this.encryptionCipher = encryptionCipher;
-    }
-
-    public SecretKey getKey() {
-        return key;
-    }
-
-    public Cipher getEncryptionCipher() {
-        return encryptionCipher;
-    }
-
-    public void setEncryptionCipher(Cipher encryptionCipher) {
-        this.encryptionCipher = encryptionCipher;
-    }
-
-    public void init() {
+    // Clave de encriptación / desencriptación
+    public SecretKeySpec crearClave(String llave) {
         try {
-            KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-            keyGenerator.init(KEY_SIZE);
-            key = keyGenerator.generateKey();
+            byte[] cadena = llave.getBytes("UTF-8");
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            cadena = md.digest(cadena);
+            cadena = Arrays.copyOf(cadena, 16);
+            SecretKeySpec secretKeySpec = new SecretKeySpec(cadena, "AES");
+            return secretKeySpec;
         } catch (Exception e) {
-            e.printStackTrace(); 
+            return null;
         }
     }
 
-    public String encrypt(String DataToEncrypt) throws Exception {
-        byte[] dataInBytes = DataToEncrypt.getBytes();
-        encryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
-        encryptionCipher.init(Cipher.ENCRYPT_MODE, key);
-        byte[] encryptedBytes = encryptionCipher.doFinal(dataInBytes);
-        return encode(encryptedBytes);
+    public AES_ENCRYPTION() {
     }
 
-    public String decrypt(String encryptedData) throws Exception {
-        byte[] dataInBytes = decode(encryptedData);
-        Cipher decryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
-        GCMParameterSpec spec = new GCMParameterSpec(DATA_LENGTH, encryptionCipher.getIV());
-        decryptionCipher.init(Cipher.DECRYPT_MODE, key, spec);
-        byte[] decryptedBytes = decryptionCipher.doFinal(dataInBytes);
-        return new String(decryptedBytes);
+    // Encriptar
+    public String encrypt(String encriptar) {
+        try {
+            SecretKeySpec secretKeySpec = crearClave(LLAVE);
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+
+            byte[] cadena = encriptar.getBytes("UTF-8");
+            byte[] encriptada = cipher.doFinal(cadena);
+            String cadena_encriptada = Base64.getEncoder().encodeToString(encriptada);  // Cambio aquí
+            return cadena_encriptada;
+        } catch (Exception e) {
+            return "";
+        }
     }
 
-    private String encode(byte[] data) {
-        return Base64.getEncoder().encodeToString(data);
+    // Des-encriptación
+    public String decrypt(String desencriptar) {
+        try {
+            SecretKeySpec secretKeySpec = crearClave(LLAVE);
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
+
+            byte[] cadena = Base64.getDecoder().decode(desencriptar);  // Cambio aquí
+            byte[] desencriptacioon = cipher.doFinal(cadena);
+            String cadena_desencriptada = new String(desencriptacioon);
+            return cadena_desencriptada;
+        } catch (Exception e) {
+            return "";
+        }
     }
 
-    private byte[] decode(String data) {
-        return Base64.getDecoder().decode(data);
-    }  
+    public static void main(String[] args) {
+        String encriptada = "";
+        String aEnccriptar = "";
+
+        AES_ENCRYPTION main = new AES_ENCRYPTION();
+
+        aEnccriptar = JOptionPane.showInputDialog("Ingresa la cadena a encriptar: ");
+
+        encriptada = main.encrypt(aEnccriptar);
+
+        JOptionPane.showMessageDialog(null, encriptada);
+        JOptionPane.showMessageDialog(null, main.decrypt(encriptada));
+    }
 }
